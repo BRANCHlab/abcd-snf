a8293e3e714c50c6281ae52d09bc175240c0fb39
psvelayudhan <psvelayu@gmail.com>
2022-11-09 15:42:53 -0500

    qol: refer to data (raw and processed) through clearer symbolic links

---
+++ raw-abcd/link_abcd.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/trauma/abcd_otbi01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_ssphy01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_ssphp01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/demographics/pdem02.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_mx01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_smrip10201.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_mrisdp10201.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_drsip201.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-gord-cor/abcd_betnet02.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-subc-cor/mrirscor02.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-temp-var/abcd_mrirstv02.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/parenting/fes02.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/parenting/abcd_fes01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/psb01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/abcd_psb01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/social-adjustment/abcd_ysr01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/stq01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/abcd_stq01.txt .
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/abcd_asrs01.txt .
