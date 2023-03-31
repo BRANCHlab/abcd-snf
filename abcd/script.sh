1cda3ce012cfe26cc5f812830d7abdff08b605be
psvelayudhan <psvelayu@gmail.com>
2023-03-31 15:16:02 -0400

    soem renaming

---
+++ abcd/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ls [0-9]*
+
+Tina worked alongside two other graduate students to design, write up and publish a research protocol for submission at the "Undergraduate Research in Natural and Clinical Science and Technology" (URNCST) journal. This research project was done as a part of UNRCST's journal mentoring program. As Tina's supervisor, I requested the topic be on Astrocyte Transplantation therapy. Tina showed great focus and dedication to this project and put in considerable effort. Tina was able to rapidly familiarize herself with the field and effectively lead her group of 3 undergraduate students to prepare and publish a great research protocol. 
