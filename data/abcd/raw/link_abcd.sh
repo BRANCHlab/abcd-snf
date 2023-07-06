6960909aa349d1e9d6122c6e23876c513637bd85
psvelayudhan <psvelayu@gmail.com>
2023-07-06 11:30:50 -0400

    symbolic link of otbi

--- data/abcd/raw/link_abcd.sh
+++ data/abcd/raw/link_abcd.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/trauma/abcd_otbi01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/trauma/abcd_tbi01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_ssphy01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_ssphp01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/demographics/pdem02.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_mx01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_smrip10201.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_mrisdp10201.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_drsip201.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-gord-cor/abcd_betnet02.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-subc-cor/mrirscor02.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-temp-var/abcd_mrirstv02.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/parenting/fes02.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/parenting/abcd_fes01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/psb01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/abcd_psb01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/abcd_cbcls01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/abcd_cbcl01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/social-adjustment/abcd_ysr01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/stq01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/abcd_stq01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/abcd_asrs01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/activity/abcd_yrb01.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/activity/abcd_saiq02.txt .
-ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/neurosignal-recordings/mriqcrp10301.txt .
+# 4.0 Release
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/trauma/abcd_otbi01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/trauma/abcd_tbi01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_ssphy01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_ssphp01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/demographics/pdem02.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/med-history/abcd_mx01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_smrip10201.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_mrisdp10201.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/evaluated-data/abcd_drsip201.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-gord-cor/abcd_betnet02.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-subc-cor/mrirscor02.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/2.0/tabular-MRI/rsfMRI-data/rsfmri-temp-var/abcd_mrirstv02.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/parenting/fes02.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/parenting/abcd_fes01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/psb01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/abcd_psb01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/abcd_cbcls01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/behaviour/abcd_cbcl01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/social-adjustment/abcd_ysr01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/stq01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/abcd_stq01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/questionnaire/abcd_asrs01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/activity/abcd_yrb01.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/activity/abcd_saiq02.txt .
+# ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/4.0/neurosignal-recordings/mriqcrp10301.txt .
+
+# 5.0 Release
+ln -s /home/prashanth/mnt/wheeler-lab/lab-data/abcd/5.0/core/physical-health/ph_p_otbi.csv .
