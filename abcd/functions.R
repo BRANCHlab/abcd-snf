9be1174978318836afb9cb36984c7ee7e99eaca6
psvelayudhan <psvelayu@gmail.com>
2024-02-01 15:11:47 -0500

    lots of new updated plots

--- abcd/functions.R
+++ abcd/functions.R
@@ -835,3 +835,137 @@ cluster_d <- function(data, main_group, ref_group, var) {
     names(results) <- c("d", "lower", "upper")
     return(results)
 }
+
+manhattan_plot <- function(data,
+                           key_mode = FALSE,
+                           threshold = NULL,
+                           bonferroni_line = FALSE,
+                           data_list,
+                           mc_labels = NULL,
+                           colours = NULL) {
+    row_id <- ""
+    variable <- ""
+    pval <- ""
+    domain <- ""
+    mean_p <- ""
+    mc_label <- ""
+    sd_p <- ""
+    print(5)
+    var_cols <- colnames(data)[endsWith(colnames(data), "_p")]
+    data[, var_cols] <- data[, var_cols] |>
+        apply(
+            MARGIN = 2,
+            FUN = function(x) {
+                -log10(x)
+                raw_p <- -log10(x)
+                new_x <- dplyr::case_when(
+                    raw_p > 5 ~ 5,
+                    #raw_p > 4 ~ 4,
+                    #raw_p > 3 ~ 3,
+                    #raw_p > 2 ~ 2,
+                    #raw_p > 1 ~ 1,
+                    #raw_p > 0 ~ 0,
+                    TRUE ~ raw_p
+                )
+                new_x
+            }
+        )
+    data$"row_id" <- factor(data$"row_id")
+    data$"mc_label" <- factor(data$"mc_label")
+    data <- data |>
+        tidyr::pivot_longer(
+            !(c(row_id, mc_label)),
+            names_to = "variable",
+            values_to = "pval"
+        ) |>
+        data.frame()
+    summary_data <- data |>
+        dplyr::group_by(
+            mc_label,
+            variable
+        ) |>
+        dplyr::summarize(
+            mean_p = mean(pval),
+            sd_p = sd(pval),
+            se_p = sd_p / sqrt(dplyr::n()),
+            .groups = "drop"
+        )
+    summary_data$"variable" <- sub("_p$", "", summary_data$"variable")
+    ###########################################################################
+    # Merge the summmary plot with domain information from the data_list
+    ###########################################################################
+    dl_metadata <- data_list_metadata(data_list) |> dplyr::select(-"type")
+    summary_data <- merge(
+        summary_data,
+        dl_metadata,
+        by.x = "variable",
+        by.y = "name",
+        all.x = TRUE
+    )
+    summary_data <- summary_data |> dplyr::arrange(domain)
+    summary_data$"variable" <- factor(
+        summary_data$"variable",
+        levels = unique(summary_data$"variable")
+    )
+    if (!is.null(mc_labels)) {
+        summary_data <- summary_data[summary_data$"mc_label" %in% mc_labels, ]
+        summary_data$"mc_label" <- factor(
+            summary_data$"mc_label",
+            levels = mc_labels
+        )
+    }
+    print(summary_data)
+    plot <- summary_data |>
+        ggplot2::ggplot(ggplot2::aes(x = domain, y = mean_p)) +
+        ggplot2::geom_jitter(
+            mapping = ggplot2::aes(
+                group = domain,
+                x = variable,
+                y = mean_p,
+                colour = domain
+            ),
+            height = 0,
+            width = 0,
+            size = 5
+        ) +
+        ggplot2::labs(
+            x = "Variable",
+            y = expression("-log"[10] * "(p)"),
+            colour = "Domain"
+        ) +
+        ggplot2::ylim(0, 5) +
+        ggplot2::theme_bw() +
+        ggplot2::labs(y = "Discretized -log10(p)") +
+        ggplot2::theme(
+            axis.text.x = ggplot2::element_text(
+                angle = 90,
+                vjust = 0.5,
+                hjust = 1
+            ),
+            plot.title = ggplot2::element_text(hjust = 0.5)
+        )
+    if (!is.null(colours)) {
+        plot <- plot + ggplot2::scale_colour_manual(values = colours)
+    }
+    plot <- plot + ggplot2::facet_grid(mc_label ~ .)
+    if (!is.null(threshold)) {
+        plot <- plot + ggplot2::geom_hline(
+            yintercept = -log10(threshold),
+            linetype = "dashed",
+            colour = "red"
+        )
+        if (bonferroni_line) {
+            plot <- plot + ggplot2::geom_hline(
+                yintercept = -log10(threshold / nrow(data)),
+                linetype = "dashed",
+                colour = "black"
+            )
+        }
+    } else if (bonferroni_line) {
+        stop(
+            "Please specify threshold p-value which will be used",
+            " for calculting the Bonferroni-corrected line."
+        )
+    }
+    return(plot)
+}
