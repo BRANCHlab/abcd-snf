d3f20d6c85810669807d36a9f80dbead6ab25369
psvelayudhan <psvelayu@gmail.com>
2025-02-07 16:26:29 -0500

    finished reruning 01_baseline_extraction.qmd

--- abcd/helper_functions.R
+++ abcd/helper_functions.R
@@ -4,7 +4,7 @@ build_dls <- function(parsed_imputations) {
     data_lists <- lapply(
         seq_len(5),
         function(x) {
-            generate_data_list(
+            data_list(
                 list(
                     parsed_imputations$"as_mem_daze"[[x + 1]],
                     "as_mem_daze",
@@ -157,11 +157,138 @@ build_dls <- function(parsed_imputations) {
     return(data_lists)
 }
 
+build_unij_dls <- function(parsed_imputations) {
+    data_lists <- lapply(
+        seq_len(5),
+        function(x) {
+            data_list(
+                list(
+                    parsed_imputations$"d_interview_age"[[x + 1]],
+                    "d_age",
+                    "D",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"d_sex"[[x + 1]],
+                    "d_sex",
+                    "D",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"d_income"[[x + 1]],
+                    "d_income",
+                    "D",
+                    "ordinal"
+                ),
+                list(
+                    parsed_imputations$"d_race"[[x + 1]],
+                    "d_race",
+                    "D",
+                    "categorical"
+                ),
+                list(
+                    parsed_imputations$"d_pubertal_status"[[x + 1]],
+                    "d_pubertal",
+                    "D",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"mh_headaches"[[x + 1]],
+                    "mh_headaches",
+                    "MH",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"sm_subc_v_qc"[[x + 1]],
+                    "sm_subc_v",
+                    "N",
+                    "continuous"
+                ),
+                list(
+                    parsed_imputations$"sm_cort_t_qc"[[x + 1]],
+                    "sm_cort_t",
+                    "N",
+                    "continuous"
+                ),
+                list(
+                    parsed_imputations$"sm_cort_sa_qc"[[x + 1]],
+                    "sm_cort_sa",
+                    "N",
+                    "continuous"
+                ),
+                list(
+                    parsed_imputations$"dm_all_wmnd_qc"[[x + 1]],
+                    "dm_all_wmnd",
+                    "N",
+                    "continuous"
+                ),
+                list(
+                    parsed_imputations$"rm_gord_cor_qc"[[x + 1]],
+                    "rm_gord_cor",
+                    "N",
+                    "continuous"
+                ),
+                list(
+                    parsed_imputations$"rm_subc_cor_qc"[[x + 1]],
+                    "rm_subc_cor",
+                    "N",
+                    "continuous"
+                ),
+                list(
+                    parsed_imputations$"p_friends"[[x + 1]],
+                    "p_friends",
+                    "P",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"p_screen_time"[[x + 1]],
+                    "p_screen_time",
+                    "P",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"p_sports"[[x + 1]],
+                    "p_sports",
+                    "P",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"p_exercise"[[x + 1]],
+                    "p_exercise",
+                    "P",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"p_prosocial"[[x + 1]],
+                    "p_prosocial",
+                    "P",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"p_fam_fn"[[x + 1]],
+                    "p_fam_fn",
+                    "P",
+                    "discrete"
+                ),
+                list(
+                    parsed_imputations$"p_parent_psych"[[x + 1]],
+                    "p_parent_psych",
+                    "P",
+                    "discrete"
+                ),
+                uid = "subjectkey"
+            )
+        }
+    )
+    names(data_lists) <- paste0("imp_", seq_len(5))
+    return(data_lists)
+}
+
 build_ols <- function(parsed_imputations) {
     oom_lists <- lapply(
         seq_len(5),
         function(x) {
-            generate_data_list(
+            data_list(
                 list(
                     parsed_imputations$"cbcl"[[x + 1]],
                     "cbcl",
